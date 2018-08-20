b18272030c82a492bdcfbca91b07e1f513eac77a
Alireza Fatemi <alirezafa@rocketmail.com>
2018-08-20 11:54:46 +0430

    Refactor.

--- src/reducers/LoginPageReducer.js
+++ src/reducers/LoginPageReducer.js
@@ -1,5 +1,5 @@
-import {EMAIL_CHANGED, PASSWORD_CHANGED, MODE_CHANGED,LOGIN_FAIL} from '../actions/LoginPageActions';
-import { LoginPageModes} from "../config/LoginPageModes";
+import {LoginPageActions} from '../actions/LoginPageActions';
+import {LoginPageModes} from "../config/LoginPageModes";
 
 const INITIAL_STATE = {
     email: '',
@@ -8,6 +8,7 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
+    const {EMAIL_CHANGED, PASSWORD_CHANGED, MODE_CHANGED, LOGIN_FAIL} = LoginPageActions;
     switch (action.type) {
         case EMAIL_CHANGED:
             return {...state, email: action.payload};
@@ -16,7 +17,7 @@ export default (state = INITIAL_STATE, action) => {
         case MODE_CHANGED:
             return {...state, mode: action.payload};
         case LOGIN_FAIL:
-            return {...state,mode:LoginPageModes.ERROR};
+            return {...state, mode: LoginPageModes.ERROR};
         default:
             return state;
     }
