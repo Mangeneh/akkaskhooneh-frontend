e222173fcd4e487905055c5a8f08d7c4a0029f25
Yasaman <ys.jafari98@gmail.com>
2018-08-20 11:17:36 +0430

    Some bugs fixed.

--- App.js
+++ App.js
@@ -7,17 +7,27 @@ import Login from "./src/pages/Login";
 import SignUp from "./src/pages/SignUp";
 import SignUpComplete from "./src/pages/SignUpComplete";
 import Profile from './src/pages/Profile'
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+ 
+const client = axios.create({ //all axios can be used, shown in axios documentation
+  baseURL:'192.168.11.140/us',
+  responseType: 'json'
+});
+
+let store = createStore(
+    rootReducer,
+    {},
+    applyMiddleware(
+        axiosMiddleware(client) //second parameter options can optionally contain onSuccess, onError, onComplete, successSuffix, errorSuffix
+  )
+)
 
 export default class App extends Component {
     render() {
         return (
-<<<<<<< HEAD
-            <Provider store={createStore(rootReducer)}>
-                <Profile />
-=======
-            <Provider store={createStore(rootReducer,{}, applyMiddleware(ReduxThunk))}>
+            <Provider store={store}>
                 <Login/>
->>>>>>> 7f32b87beccc45c8cba47bc4cb66c15e153c3aa6
             </Provider>
         );
     }
