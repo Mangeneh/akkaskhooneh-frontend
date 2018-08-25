a44f2b3a56430590e13d236f8ca2668061056222
Alireza Fatemi <alirezafa@rocketmail.com>
2018-08-25 15:47:25 +0430

    Bug Fix!

--- src/pages/profileSettings/ProfileSettings.js
+++ src/pages/profileSettings/ProfileSettings.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import {Container, Item, Header, Right, Left, Icon, Body} from 'native-base';
+import {Item, Right, Left, Icon} from 'native-base';
 import {View, TouchableOpacity, StyleSheet, StatusBar, Text} from 'react-native'
 import {Strings} from '../../config/Strings';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {Colors} from "../../config/Colors";
 import Constants from "../../config/Constants";
 import BackHeader from "../../components/BackHeader";
+import Fonts from "../../config/Fonts";
 
 export default class ProfileSettings extends Component {
     static navigationOptions = {
@@ -15,37 +15,34 @@ export default class ProfileSettings extends Component {
     render() {
         const {SIGN_OUT, CHANGE_PASS} = Strings;
         return (
-            <KeyboardAwareScrollView>
+            <View style={{flex: 1}}>
                 <StatusBar
                     barStyle="light-content"
                     backgroundColor={Colors.BASE}
                 />
                 <BackHeader onBackPress={this.onBackPress.bind(this)} title={Strings.PROFILE_SETTINGS}/>
-                <Header />
-                <Container style={{backgroundColor: 'white', flex: 1}}>
-                    <View style={{flex: 4}}>
-                    <Item onPress={this.onChangePassPressed.bind(this)}>
-                        <Left>
-                            <TouchableOpacity>
-                                <Icon type={"Ionicons"} name='ios-arrow-back' style={{color: Colors.ACCENT}}/> 
-                            </TouchableOpacity> 
-                        </Left>
-                        <Right>
-                            <TouchableOpacity>
-                                <Text style={{fontFamily: Constants.NORMAL_FONT, fontSize: Constants.TEXT_NORMAL_SIZE, color: Colors.TEXT,}}>
-                                    {CHANGE_PASS}
-                                </Text>
-                            </TouchableOpacity>
-                        </Right>
-                    </Item>
+                <View style={{backgroundColor: 'white', flex: 1}}>
+                    <View>
+                        <Item onPress={this.onChangePassPressed.bind(this)}>
+                            <Left>
+                                <TouchableOpacity onPress={this.onChangePassPressed.bind(this)}>
+                                    <Icon type={"Ionicons"} name='ios-arrow-back' style={{color: Colors.ACCENT}}/>
+                                </TouchableOpacity>
+                            </Left>
+                            <Right>
+                                <TouchableOpacity onPress={this.onChangePassPressed.bind(this)}>
+                                    <Text style={styles.text}>
+                                        {CHANGE_PASS}
+                                    </Text>
+                                </TouchableOpacity>
+                            </Right>
+                        </Item>
                     </View>
-                    <View style={{flex: 2}}>
-                        <TouchableOpacity style={{ marginBottom: 24}}>
-                            <Text style={styles.text}>{SIGN_OUT}</Text>
-                        </TouchableOpacity>
-                    </View>
-                </Container>
-            </KeyboardAwareScrollView>
+                </View>
+                <TouchableOpacity style={{marginBottom: 0}}>
+                    <Text style={styles.exit}>{SIGN_OUT}</Text>
+                </TouchableOpacity>
+            </View>
         );
     }
 
@@ -67,12 +64,15 @@ const styles = StyleSheet.create({
         borderRadius: Constants.TEXT_BOX_RADIUS,
         elevation: Constants.TEXT_BOX_ELEVATION
     },
-
     text: {
-        fontFamily: Constants.NORMAL_FONT,
+        fontFamily: Fonts.NORMAL_FONT,
+        fontSize: 20,
+        color: Colors.TEXT,
+    },
+    exit: {
+        fontFamily: Fonts.NORMAL_FONT,
         fontSize: Constants.TEXT_NORMAL_SIZE,
         color: Colors.TEXT,
-        textAlign: 'center',
-        alignSelf: 'center'
-    }
+        textAlign: 'center'
+    },
 });
