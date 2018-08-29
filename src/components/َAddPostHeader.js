3b0fb9df0c1275348401554e1018468c376c6584
Alireza Fatemi <alirezafa@rocketmail.com>
2018-08-29 14:55:30 +0430

    Overall Improvements.

---
+++ src/components/َAddPostHeader.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {Header, Left, Body, Right, Icon, Title, Text} from 'native-base';
+import {TouchableOpacity, View} from 'react-native';
+import {Colors, Strings, Fonts} from '../config';
+
+class AddPostHeader extends Component {
+    render() {
+        return (
+            <View>
+                <Header androidStatusBarColor={Colors.BASE} style={{backgroundColor: Colors.BASE}}>
+                    <Left style={{flex: 1, marginLeft: 16}}>
+                        <TouchableOpacity onPress={() => {
+                            this.props.onEditPress()
+                        }}>
+                            <Text style={{color: 'white', fontFamily: Fonts.NORMAL_FONT}}>{Strings.EDIT}</Text>
+                        </TouchableOpacity>
+                    </Left>
+                    <Body style={{flex: 3}}>
+                    <Title style={{
+                        alignSelf: 'center',
+                        color: 'white',
+                        fontFamily: Fonts.NORMAL_FONT
+                    }}>{this.props.username}</Title>
+                    </Body>
+                    <Right style={{flex: 1, marginRight: 16}}>
+                        <TouchableOpacity onPress={() => {
+                            this.props.onSettingsPress()
+                        }}>
+                            <Icon type={'MaterialCommunityIcons'} name='ship-wheel' style={{color: 'white'}}/>
+                        </TouchableOpacity>
+                    </Right>
+                </Header>
+            </View>
+        );
+    }
+}
---
+++ src/pages/addPostInfo/AddPostInfo.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react';
+import {Container, Tab, Tabs} from 'native-base';
+import {View, StatusBar, Image, FlatList, ActivityIndicator, Dimensions, StyleSheet} from 'react-native';
+import {connect} from 'react-redux';
+import NavigationService from '../../NavigationService';
+import {Colors, Fonts, Strings} from '../../config';
+import {ProfileHeader} from '../../components';
+
+const WIDTH = Dimensions.get('window').width;
+
+class Profile extends Component {
+    static navigationOptions = {
+        header: null,
+    };
+
+    componentDidMount() {
+        setTimeout(this._tabs.goToPage.bind(this._tabs, 1));
+        this.updatePhotos();
+    }
+
+    render() {
+        const {} = Strings;
+        return (
+
+        );
+    }
+
+    renderPhoto(item, index) {
+        return (
+            <View style={index % 2 === 0 ? styles.evenPhoto : styles.oddPhoto}>
+                <Image source={{uri: item.picture}} resizeMode={'stretch'}
+                       style={{width: WIDTH / 2, height: WIDTH / 2}}/>
+            </View>
+        );
+    }
+
+    updatePhotos() {
+        if (!this.props.fetchStatus) {
+            this.props.getPhotosNextPage(this.props.photosNext);
+        }
+    }
+
+    onEditPress() {
+        NavigationService.navigate('ProfileEdit');
+    }
+
+    onSettingsPress() {
+        NavigationService.navigate('ProfileSettings');
+    }
+}
+
+const styles = StyleSheet.create({
+    evenPhoto: {
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+        marginRight: 4,
+        marginLeft: 8,
+        marginBottom: 8,
+        borderRadius: 10,
+        overflow: 'hidden'
+    },
+    oddPhoto: {
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+        marginBottom: 8,
+        marginRight: 8,
+        marginLeft: 4,
+        borderRadius: 10,
+        overflow: 'hidden'
+    }
+});
+
+const mapStateToProps = (state) => ({
+    username: state.userInfo.user.username,
+    photos: state.profilePage.photos,
+    photosNext: state.profilePage.photosNext,
+    isLoading: state.profilePage.isLoading,
+    fetchStatus: state.profilePage.fetchStatus
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    getPhotosNextPage: (photosNext) => dispatch(getPhotosNextPage(photosNext))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
