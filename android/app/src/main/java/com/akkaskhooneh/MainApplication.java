451580a78d5438d019267a5047a07959b606fe72
Alireza Fatemi <alirezafa@rocketmail.com>
2018-08-31 20:13:52 +0430

    Configured Contacts!

--- android/app/src/main/java/com/akkaskhooneh/MainApplication.java
+++ android/app/src/main/java/com/akkaskhooneh/MainApplication.java
@@ -3,6 +3,7 @@ package com.akkaskhooneh;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.wix.RNCameraKit.RNCameraKitPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeContacts(),
             new RNCameraKitPackage(),
             new PickerPackage(),
             new VectorIconsPackage()
--- src/pages/addFriends/AddFriends.js
+++ src/pages/addFriends/AddFriends.js
@@ -1,31 +1,57 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {FlatList, View} from 'react-native';
 import {Container, Header, Item, Input, Icon, Button, Text} from 'native-base';
+import Contacts from 'react-native-contacts';
 import BackHeader from "../../components/BackHeader";
 import {Colors, Constants, Fonts, Strings} from "../../config";
 import {CustomStatusBar} from "../../components";
 
 class AddFriends extends Component {
+
+    constructor(props) {
+        super(props);
+        Contacts.getAll((err, contacts) => {
+            if (err) throw err;
+            // contacts returned
+            console.log(contacts);
+            this.contacts = contacts;
+        })
+    }
+
     render() {
         const {TEXT_BOX_FONT_SIZE, TEXT_BOX_RADIUS, TEXT_BOX_ELEVATION} = Constants;
         return (
             <Container>
                 <CustomStatusBar/>
                 <BackHeader title={Strings.INVITE_FRIENDS} onBackPress={() => this.props.navigation.goBack()}/>
-                <View style={{marginRight: 32,marginLeft: 32,marginTop: 16}}>
+                <View style={{marginRight: 32, marginLeft: 32, marginTop: 16}}>
                     <Item rounded style={{
                         alignSelf: 'center',
                         backgroundColor: Colors.WHITE_BACK,
                         borderRadius: TEXT_BOX_RADIUS
                     }}>
                         <Icon name="ios-people"/>
-                        <Input placeholder={Strings.SEARCH_CONTACT} style={{textAlign: 'right', fontSize: TEXT_BOX_FONT_SIZE}}
+                        <Input placeholder={Strings.SEARCH_CONTACT}
+                               style={{textAlign: 'right', fontSize: TEXT_BOX_FONT_SIZE}}
                                fontFamily={Fonts.NORMAL_FONT}/>
                         <Icon name="ios-search"/>
                     </Item>
                 </View>
+                <View style={{backgroundColor: Colors.WHITE_BACK, flex: 1}}>
+                    <FlatList
+                        style={{width: '100%', marginTop: 8}}
+                        numColumns={2}
+                        keyExtractor={(item, index) => item.id}
+                        data={this.contacts}
+                        renderItem={({item, index}) => this.renderContact(item, index)}
+                    />
+                </View>
             </Container>
         );
+    }
+
+    renderContact(item, index) {
+
     }
 }
 
