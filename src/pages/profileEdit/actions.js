97c7ae51971b60cd680b698598cddaee1c1e0d58
Alireza Fatemi <alirezafa@rocketmail.com>
2018-09-06 17:52:06 +0430

    A little Refactor!(More To Come!;)

--- src/pages/profileEdit/actions.js
+++ src/pages/profileEdit/actions.js
@@ -1,47 +1,41 @@
-import {Server} from "../../config";
+import { Server } from '../../config';
 
 export const Actions = {
-    NORMALIZE: 'PROFILE_EDIT_NORMALIZE',
-    IMAGE_CHANGED: 'PROFILE_EDIT_IMAGE_CHANGED',
-    EDIT_PROFILE: 'EDIT_PROFILE',
-    EDIT_PROFILE_SUCCESS: 'EDIT_PROFILE_SUCCESS',
-    EDIT_PROFILE_FAIL: 'EDIT_PROFILE_FAIL',
-    CHANGE_PROFILE_PIC: 'CHANGE_PROFILE_PIC',
-    CHANGE_PROFILE_PIC_SUCCESS: 'CHANGE_PROFILE_PIC_SUCCESS',
-    CHANGE_PROFILE_PIC_FAIL: 'CHANGE_PROFILE_PIC_FAIL',
+  NORMALIZE: 'PROFILE_EDIT_NORMALIZE',
+  IMAGE_CHANGED: 'PROFILE_EDIT_IMAGE_CHANGED',
+  EDIT_PROFILE: 'EDIT_PROFILE',
+  EDIT_PROFILE_SUCCESS: 'EDIT_PROFILE_SUCCESS',
+  EDIT_PROFILE_FAIL: 'EDIT_PROFILE_FAIL',
+  CHANGE_PROFILE_PIC: 'CHANGE_PROFILE_PIC',
+  CHANGE_PROFILE_PIC_SUCCESS: 'CHANGE_PROFILE_PIC_SUCCESS',
+  CHANGE_PROFILE_PIC_FAIL: 'CHANGE_PROFILE_PIC_FAIL',
 };
 
-export const normalize = () => {
-    return {
-        type: Actions.NORMALIZE
-    }
-};
+export const normalize = () => ({
+  type: Actions.NORMALIZE,
+});
 
-export const editProfile = (fullName, bio) => {
-    return {
-        type: Actions.EDIT_PROFILE,
-        payload: {
-            request: {
-                method: 'POST',
-                url: Server.EDIT_PROFILE,
-                data: {
-                    fullname: fullName,
-                    bio: bio
-                }
-            }
-        }
-    };
-};
+export const editProfile = (fullName, bio) => ({
+  type: Actions.EDIT_PROFILE,
+  payload: {
+    request: {
+      method: 'POST',
+      url: Server.EDIT_PROFILE,
+      data: {
+        fullname: fullName,
+        bio,
+      },
+    },
+  },
+});
 
-export const changeProfilePic = (formData) => {
-    return {
-        type: Actions.CHANGE_PROFILE_PIC,
-        payload: {
-            request: {
-                method: 'POST',
-                url: Server.CHANGE_PROFILE_PIC,
-                data: formData
-            }
-        }
-    };
-};
+export const changeProfilePic = formData => ({
+  type: Actions.CHANGE_PROFILE_PIC,
+  payload: {
+    request: {
+      method: 'POST',
+      url: Server.CHANGE_PROFILE_PIC,
+      data: formData,
+    },
+  },
+});
