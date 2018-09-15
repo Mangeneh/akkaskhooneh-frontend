4a38ffd9a60f007e844b67f3f13db5e7cd53bdbd
Alireza Fatemi <alirezafa@rocketmail.com>
2018-09-15 20:19:30 +0430

    Improvements!

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -2,25 +2,24 @@ import { Button, Text } from 'native-base';
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
-import { FollowModes, Strings } from '../config';
+import {
+  Colors, FollowModes, Graphics, Strings,
+} from '../config';
 import { extractFollowMode } from '../helpers';
 import { strings } from '../i18n';
 import { selectProfileFollowStatus } from '../reducers/UsersReducer';
 
 class FollowButton extends Component {
   render() {
-    console.log("follow button");
-    console.log(this.props);
     const { onPress } = this.props;
     const buttonSettings = this.makeButtonSettings();
-    console.log(buttonSettings);
     return (
       <Button
         onPress={onPress}
         block
-        style={buttonSettings.style}
+        style={buttonSettings.buttonStyle}
       >
-        <Text>{buttonSettings.text}</Text>
+        <Text style={buttonSettings.textStyle}>{buttonSettings.text}</Text>
       </Button>
     );
   }
@@ -32,26 +31,58 @@ class FollowButton extends Component {
       case FollowModes.FOLLOWED:
         return {
           text: strings(Strings.FOLLOWED),
-          style: styles.followed,
+          buttonStyle: styles.buttonFollowed,
+          textStyle: styles.textFollowed,
         };
       case FollowModes.NOT_FOLLOWED:
         return {
           text: strings(Strings.FOLLOW),
-          style: styles.notFollowed,
+          buttonStyle: styles.buttonNotFollowed,
+          textStyle: styles.textNotFollowed,
         };
       case FollowModes.REQUESTED:
         return {
           text: strings(Strings.REQUESTED),
-          style: styles.requested,
+          buttonStyle: styles.buttonRequested,
+          textStyle: styles.textRequested,
         };
     }
   }
 }
 
 const styles = StyleSheet.create({
-  followed: {},
-  notFollowed: {},
-  requested: {},
+  buttonFollowed: {
+    alignSelf: 'center',
+    width: Graphics.FOLLOW_BUTTON_WIDTH,
+    height: Graphics.FOLLOW_BUTTON_HEIGHT,
+    borderRadius: Graphics.FOLLOW_BUTTON_BORDER_RADIUS,
+    backgroundColor: Colors.ACCENT,
+  },
+  buttonNotFollowed: {
+    alignSelf: 'center',
+    width: Graphics.FOLLOW_BUTTON_WIDTH,
+    height: Graphics.FOLLOW_BUTTON_HEIGHT,
+    borderRadius: Graphics.FOLLOW_BUTTON_BORDER_RADIUS,
+    borderWidth: Graphics.FOLLOW_BUTTON_BORDER_WIDTH,
+    borderColor: Colors.ACCENT,
+    backgroundColor: 'white',
+  },
+  buttonRequested: {
+    alignSelf: 'center',
+    width: Graphics.FOLLOW_BUTTON_WIDTH,
+    height: Graphics.FOLLOW_BUTTON_HEIGHT,
+    borderRadius: Graphics.FOLLOW_BUTTON_BORDER_RADIUS,
+    backgroundColor: Colors.ACCENT,
+  },
+  textFollowed: {
+    color: 'white',
+  },
+  textNotFollowed: {
+    color: Colors.ACCENT,
+  },
+  textRequested: {
+    color: 'white',
+  },
 });
 
 const mapStateToProps = (state, ownProps) => {
