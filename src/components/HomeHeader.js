928fc565cf0c1141a4c1598d514d7abb9d767288
Alireza Fatemi <alirezafa@rocketmail.com>
2018-09-16 18:50:06 +0430

    Removed Not Implemented Features!

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -9,19 +9,10 @@ import { selectHomePostsIsLoading } from '../reducers/PostsReducer';
 
 class HomeHeader extends Component {
   render() {
-    const { onAddFriendsPress, title } = this.props;
+    const { title } = this.props;
     return (
       <Header androidStatusBarColor={Colors.BASE} style={{ backgroundColor: Colors.BASE }}>
-        <Left style={{
-          flex: 1,
-          marginLeft: 16,
-        }}
-        >
-          <TouchableOpacity onPress={onAddFriendsPress}>
-            <Icon name="user-plus" type="Feather" style={{ color: 'white' }} />
-          </TouchableOpacity>
-        </Left>
-        <Body style={{ flex: 3 }}>
+        <Body style={{ flex: 1 }}>
           <Title style={{
             alignSelf: 'center',
             color: 'white',
@@ -30,24 +21,8 @@ class HomeHeader extends Component {
             {title}
           </Title>
         </Body>
-        <Right style={{
-          flex: 1,
-          marginRight: 16,
-        }}
-        >
-          {this.renderLoader()}
-        </Right>
       </Header>
     );
-  }
-
-  renderLoader() {
-    const { postsIsLoading } = this.props;
-    return <View />;
-    if (postsIsLoading) {
-      return <ActivityIndicator size="large" />;
-    }
-    return <View />;
   }
 }
 
